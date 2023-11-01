bbf50f3c5d3e5fa07686495ca401df18023c2144
Safi_Amin <Safi.amini786@gmail.com>
2023-10-31 19:43:08 -0500

    create DeckModule_StepDef

---
+++ src/test/java/com/cloud/step_definitions/DeckModule_StepDefs.java
@@ -0,0 +1,13 @@
+package com.cloud.step_definitions;
+
+import com.cloud.pages.LoginPage;
+import com.cloud.utilities.ConfigurationReader;
+import com.cloud.utilities.Driver;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DeckModule_StepDefs {
+
+
+}
